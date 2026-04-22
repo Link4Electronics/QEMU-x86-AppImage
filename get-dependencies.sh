3bcd48f0ba539b6fc9959d85b6069034cefb3213
Link4Electronics <link4electronics@gmail.com>
2026-04-22 08:13:17 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -23,7 +23,6 @@ pacman -Syu --noconfirm --overwrite '/usr/share/qemu/*' \
     qemu-desktop     \
     qemu-user        \
     qemu-user-binfmt \
-    spice-gtk        \
     swtpm            \
     virtiofsd
 
@@ -32,7 +31,6 @@ echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-make-aur-package zenity-rs-bin
-make-aur-package quickemu
+#make-aur-package PACKAGENAME
 
 # If the application needs to be manually built that has to be done down here
